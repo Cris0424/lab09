980b770a86633a65430e0dc73486f82997fd4e88
Cristian Qorri <cristian.qorri@studio.unibo.it>
2024-11-18 12:20:17 +0100

    implementaton of interface

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,39 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+    public static final String HOME = System.getProperty("user.home");
+    public static final String DEFAULT_FILE = "output.txt";
+    File curruntFile = new File(HOME + File.separator + DEFAULT_FILE);
 
+    public void setCurrentFile(final File file) {
+        if ((file.exists()) && (file.isFile())) {
+            curruntFile = file;
+        } else {
+            throw new IllegalArgumentException("file not exist");
+        }
+    }
+
+    public File getCurrentFile() {
+        return curruntFile;
+    }
+
+    public String getCurrentPath() {
+        return curruntFile.getAbsolutePath();
+    }  
+
+    public void saveString(final String string) {
+        try (PrintStream ps = new PrintStream(curruntFile, StandardCharsets.UTF_8)) {
+            ps.println(string);
+        } catch (IOException e) {
+            System.err.println("Error");
+        }
+    }
 }
