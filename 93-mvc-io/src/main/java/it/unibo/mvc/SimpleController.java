cf75a69b1c7b326cf611d1465488094bed27548e
Cristian Qorri <cristian.qorri@studio.unibo.it>
2024-11-19 10:58:30 +0100

    added actionListener for button print

--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,5 +1,6 @@
 package it.unibo.mvc;
 
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,7 +13,7 @@ public final class SimpleController implements Controller {
     List<String> historyList = new LinkedList<>();
 
     @Override
-    public void setNextStringToPrint(String nextString) {
+    public void setNextStringToPrint(String nextString){
         if (nextString.equals(null)) {
             throw new NullPointerException("null values are not acceptable");
         } else {
--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,10 +1,18 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * A very simple program using a graphical interface.
@@ -19,6 +27,35 @@ public final class SimpleGUI {
         canvas.setLayout(new BorderLayout());
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        final JTextField textField = new JTextField();
+        canvas.add(textField, BorderLayout.NORTH);
+
+        final JTextArea textArea = new JTextArea();
+        canvas.add(textArea, BorderLayout.CENTER);
+
+        final JPanel canvas2 = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        canvas.add(canvas2, BorderLayout.SOUTH);
+
+        final JButton printButton = new JButton("print");
+        canvas2.add(printButton,BorderLayout.SOUTH);
+
+        final JButton historyButton = new JButton("show history");
+        canvas2.add(historyButton,BorderLayout.SOUTH);
+
+        /*
+         * Handlers
+         */
+        printButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                controller.setNextStringToPrint(textField.getText());
+                System.out.println(controller.getNextStringToPrint());
+            }
+            
+        });
+
     }
 
     private void display() {
