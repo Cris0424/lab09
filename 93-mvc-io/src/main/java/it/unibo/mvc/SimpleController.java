724a3f33e00cb852ebb3bbc8d7b95398caaaf9bf
Cristian Qorri <cristian.qorri@studio.unibo.it>
2024-11-19 09:37:35 +0100

    implementation methods setNextStringToPrint, getHIstoryString, getNextSTring and printCurrentString

--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,45 @@
 package it.unibo.mvc;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 
  *
  */
 public final class SimpleController implements Controller {
+    private String nextString;
+    List<String> historyList = new LinkedList<>();
+
+    @Override
+    public void setNextStringToPrint(String nextString) {
+        this.nextString = nextString;
+        if (nextString.equals(null)) {
+            throw new NullPointerException("null values are not acceptable");
+        }
+    }
+
+    @Override
+    public String getNextStringToPrint() {
+        return this.nextString;
+    }
+
+    @Override
+    public List<String> getHistoryStringPrinted() {
+        for (String listHistoryString : historyList) {
+            System.out.println(listHistoryString);
+        }
+        return historyList;
+    }
+
+    @Override
+    public void printCurrentString() {
+        if (this.nextString.equals(null)) {
+            throw new IllegalStateException();
+        } else {
+            historyList.add(nextString);
+            System.out.println(historyList);
+        }
+    }
 
 }
